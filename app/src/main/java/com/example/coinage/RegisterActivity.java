bcc4298ccfbb95d1dad37ed7dedcc479c06afaf5
Stephanie Guan <stephanieguan@fb.com>
2022-06-22 16:00:02 -0700

    register textviews

--- app/src/main/java/com/example/coinage/RegisterActivity.java
+++ app/src/main/java/com/example/coinage/RegisterActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class RegisterActivity extends AppCompatActivity {
     public static final String TAG = "RegisterActivity";
+    private EditText etEmail;
+    private EditText etPassword;
     private Button btnFinishRegister;
 
     @Override
@@ -16,11 +25,15 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
         btnFinishRegister = findViewById(R.id.btnFinishRegister);
         btnFinishRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goHomeActivity();
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                signupUser(email, password);
             }
         });
     }
@@ -29,5 +42,22 @@ public class RegisterActivity extends AppCompatActivity {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
+    }
+
+    private void signupUser(String email, String password) {
+        ParseUser user = new ParseUser();
+        user.setUsername(email);
+        user.setPassword(password);
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    goHomeActivity();
+                } else {
+                    Log.e(TAG, "issue with register", e);
+                    Toast.makeText(RegisterActivity.this, "issue with register", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
     }
 }
