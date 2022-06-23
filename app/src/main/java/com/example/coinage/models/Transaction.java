05f1b334851db0b831004c01c32cbb5980f92fc8
Stephanie Guan <stephanieguan@fb.com>
2022-06-23 15:39:26 -0700

    transaction class and add onclick

---
+++ app/src/main/java/com/example/coinage/models/Transaction.java
@@ -0,0 +1,46 @@
+package com.example.coinage.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+import java.util.Date;
+
+@ParseClassName("Transaction")
+public class Transaction extends ParseObject {
+    public static final String KEY_DATE = "date";
+    public static final String KEY_AMOUNT = "amount";
+    public static final String KEY_CATEGORY = "category";
+    public static final String KEY_DESCRIPTION = "description";
+
+    public Date getDate() {
+        return getDate(KEY_DATE);
+    }
+
+    public void setDate(Date date) {
+        put(KEY_DATE, date);
+    }
+
+    public Number getAmount() {
+        return getNumber(KEY_AMOUNT);
+    }
+
+    public void setAmount(Number amount) {
+        put(KEY_AMOUNT, amount);
+    }
+
+    public String getCategory() {
+        return getString(KEY_CATEGORY);
+    }
+
+    public void setCategory(String category) {
+        put(KEY_CATEGORY, category);
+    }
+
+    public String getDescription() {
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public void setDescription(String description) {
+        put(KEY_DESCRIPTION, description);
+    }
+}
