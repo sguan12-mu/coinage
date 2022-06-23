42b33e021e5444a11b06e102be6b8eef5e2ec1df
Stephanie Guan <stephanieguan@fb.com>
2022-06-23 14:22:56 -0700

    setting up add purchases

--- app/src/main/java/com/example/coinage/fragments/AddFragment.java
+++ app/src/main/java/com/example/coinage/fragments/AddFragment.java
@@ -9,11 +9,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.coinage.R;
 
+import java.util.Date;
+
 public class AddFragment extends Fragment {
     public static final String TAG = "AddFragment";
+
+    private EditText etDate;
+    private EditText etAmount;
+    private EditText etCategory;
+    private EditText etDescription;
+    private Button btnAdd;
 
     public AddFragment() {
         // Required empty public constructor
@@ -28,5 +38,22 @@ public class AddFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        etDate = view.findViewById(R.id.etDate);
+        etAmount = view.findViewById(R.id.etAmount);
+        etCategory = view.findViewById(R.id.etCategory);
+        etDescription = view.findViewById(R.id.etDescription);
+        btnAdd = view.findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Date date = java.sql.Date.valueOf(etDate.getText().toString());
+                Float amount = Float.valueOf(etAmount.getText().toString());
+                String category = etCategory.getText().toString();
+                String description = etDescription.getText().toString();
+            }
+        });
+
+
     }
 }
