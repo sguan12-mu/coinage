54298f54455833999388f953459690f90e466cb9
Stephanie Guan <stephanieguan@fb.com>
2022-06-29 16:37:48 -0700

    spinner setup

--- app/src/main/java/com/example/coinage/fragments/SpendingLimitOverviewFragment.java
+++ app/src/main/java/com/example/coinage/fragments/SpendingLimitOverviewFragment.java
@@ -1,14 +1,19 @@
 package com.example.coinage.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.coinage.R;
 
--- app/src/main/java/com/example/coinage/fragments/profile/SetLimitsFragment.java
+++ app/src/main/java/com/example/coinage/fragments/profile/SetLimitsFragment.java
@@ -6,14 +6,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.coinage.R;
 
-public class SetLimitsFragment extends Fragment {
+import java.lang.reflect.Array;
+
+public class SetLimitsFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     public static final String TAG = "SetLimitsFragment";
+
+    private Spinner sCategories;
 
     public SetLimitsFragment() {
         // Required empty public constructor
@@ -28,5 +36,24 @@ public class SetLimitsFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        sCategories = view.findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.categories,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sCategories.setAdapter(adapter);
+        sCategories.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.i(TAG, String.valueOf(parent.getItemAtPosition(position)));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
