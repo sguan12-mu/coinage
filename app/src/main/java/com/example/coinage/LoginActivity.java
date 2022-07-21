2b2c08d3bef9cc95a8d29a5b2e5e6fada8fd584c
Stephanie Guan <49424605+sguan12@users.noreply.github.com>
2022-07-21 11:58:20 -0700

    pull request practice

--- app/src/main/java/com/example/coinage/LoginActivity.java
+++ app/src/main/java/com/example/coinage/LoginActivity.java
@@ -2,7 +2,6 @@ package com.example.coinage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -12,7 +11,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
