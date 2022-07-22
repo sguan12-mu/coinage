515a51a94bf3cfa807c81e606a6231bd0173999e
Stephanie Guan <49424605+sguan12@users.noreply.github.com>
2022-07-22 16:57:15 -0700

    google oauth signin

--- app/src/main/java/com/example/coinage/fragments/GoogleSheetsFragment.java
+++ app/src/main/java/com/example/coinage/fragments/GoogleSheetsFragment.java
@@ -1,19 +1,41 @@
 package com.example.coinage.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.coinage.R;
+import com.google.android.gms.auth.api.identity.GetSignInIntentRequest;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class GoogleSheetsFragment extends Fragment {
     public static final String TAG = "GoogleSheetsFragment";
+
+    private SignInButton btnGoogleSignIn;
+    private Button btnGoogleSignOut;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 0;
 
     public GoogleSheetsFragment() {
         // Required empty public constructor
@@ -26,7 +48,78 @@ public class GoogleSheetsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_google_sheets, container, false);
     }
 
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.google_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(getContext(), gso);
+
+        btnGoogleSignIn = view.findViewById(R.id.btnGoogleSignIn);
+        btnGoogleSignIn.setOnClickListener((View v) -> {
+            switch (v.getId()) {
+                case R.id.btnGoogleSignIn:
+                    signIn();
+                    break;
+            }
+        });
+
+        btnGoogleSignOut = view.findViewById(R.id.btnGoogleSignOut);
+        btnGoogleSignOut.setOnClickListener((View v) -> {
+            switch (v.getId()) {
+                case R.id.btnGoogleSignOut:
+                    signOut();
+                    break;
+            }
+        });
+    }
+
+    private void signIn() {
+        Log.i(TAG, "attempting signin");
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Log.i(TAG, "OAuth successful");
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "OAuth unsuccessful, failed code=" + e.getStatusCode());
+        }
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(getContext(), "Signed out successfully", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
